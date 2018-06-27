b7a8f70ea8ad5bb9a2322db0ab7e2527fc13b2a5
Ezra Bekele <ebekele@fb.com>
2018-06-27 10:24:38 -0700

    initial commit

---
+++ flix_demo_03/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  flix_demo_03
+//
+//  Created by Ezra Bekele on 6/27/18.
+//  Copyright © 2018 flix_demo_03. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
