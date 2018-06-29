81f5e9c4ef6e1876f18714a04d1034e2f06347af
Ezra Bekele <ebekele@fb.com>
2018-06-29 14:30:43 -0700

    bit of trailer

---
+++ flix_demo_03/Views/movieTrailer.h
@@ -0,0 +1,12 @@
+//
+//  movieTrailer.h
+//  
+//
+//  Created by Ezra Bekele on 6/29/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface movieTrailer : UIWebView
+
+@end
---
+++ flix_demo_03/Views/movieTrailer.m
@@ -0,0 +1,20 @@
+//
+//  movieTrailer.m
+//  
+//
+//  Created by Ezra Bekele on 6/29/18.
+//
+
+#import "movieTrailer.h"
+
+@implementation movieTrailer
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
